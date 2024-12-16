8dc45a925c85ed22ae103aca7658237bdb7dc7ff
nathalia <nathaliawehbi7@gmail.com>
2024-12-16 14:23:04 +0100

    filled the main files

---
+++ frontend/src/pages/StartPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './StartPage.css';
+
+function StartPage() {
+  return (
+    <div className="start-page">
+      <h1>Welcome to the Dating App</h1>
+      <p>Find your perfect match today!</p>
+    </div>
+  );
+}
+
+export default StartPage;
